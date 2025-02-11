923d3b3fa22391384945db0cdc42b4bf109460af
Anna <annanguyn99@gmail.com>
2025-02-11 22:33:39 +1100

    In progress issues

--- rovo/src/index.js
+++ rovo/src/index.js
@@ -3,18 +3,24 @@ import api, { route } from "@forge/api";
 export const getIssues = async (payload) => {
     console.log(`Payload: ${JSON.stringify(payload)}`);
 
-    const projectKey = payload.context?.jira?.projectKey || null;
-    const label = payload.label || null;
+    const projectKey = payload.projectKey || payload.context?.jira?.projectKey || null;
+
+    console.log(projectKey);
 
     if (!projectKey) {
         throw new Error("Project Key is required");
     }
 
-    console.log(`Fetching issues for project: ${projectKey} and label: ${label}`);
+    console.log(`Fetching issues for project: ${projectKey}`);
 
-    const jql = label ? `project=${projectKey} AND labels=${label}` : `project=${projectKey}`;
+    const jql = `project=${projectKey} 
+            AND status="In Progress" 
+            AND assignee=currentUser()
+            AND status changed to "In Progress" before -2w`;
     const response = await api.asUser().requestJira(route`/rest/api/3/search?jql=${jql}`);
     const data = await response.json();
+
+    // console.log(data);
     
     return extractIssueDetails(data);
 }
